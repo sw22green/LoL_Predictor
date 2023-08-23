495f8fc386becda4a42bb0d4a00a30a53b1e1720
Sheng Wang <sw875@cornell.edu>
2023-08-23 00:53:39 -0400

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,37 @@
+"""
+Data that we have
+
+    Champions:
+        Games played
+        Pick ban
+        Number of  unique players
+        WR
+        KDA
+        CS/min
+        Gold/min
+        DPS
+        KP
+        Kill share
+        Gold share
+        Role
+
+    Player stats:
+        Games played
+        Pick ban
+        WR
+        KDA
+        CS/min
+        Gold/min
+        DPS
+        KP
+        Kill share
+        Gold share
+        Role
+        Champions Played
+
+    Team stats:
+        Standing
+        Streak
+        WR
+        Previous scores against same team
+"""
---
+++ MainProgram.py
@@ -0,0 +1,25 @@
+"""
+ Ways to determine chance of winning
+
+    Team Comps
+        Synergies(steal from U.gg)
+        AD/AP
+        Tank/Mage/Assassin/Adc/Engage/Enchanter
+        Range vs engange
+
+    Player's mastery of champ
+        WR
+        Games played
+    
+    What's the meta
+        Pick/ban rate
+        Winrate
+
+    The team
+        Team's standings
+        their winstreak/lose streak
+        Player experience
+
+    
+
+ """
