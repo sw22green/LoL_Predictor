a15fcd34e616b008bbd3d49dae710e1119edaef1
Sheng Wang <sw875@cornell.edu>
2023-08-31 00:59:55 -0400

    Add files via upload

--- Data.py
+++ Data.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import csv
 import pandas as pd
+from itertools import chain
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -10,7 +11,108 @@ CSV_NAME3 = ""
 CSV_NAME4 = ""
 
 def main():
-    allData("https://lol.fandom.com/wiki/LCK/2023_Season/Spring_Season/Match_History")
+    # # Spring Season
+    # allData("https://lol.fandom.com/wiki/LCK/2023_Season/Spring_Season/Match_History")
+    # # Spring Playoff
+    # allData("https://lol.fandom.com/wiki/LCK/2023_Season/Spring_Playoffs/Match_History")
+    # # Summer Season
+    # allData("https://lol.fandom.com/wiki/LCK/2023_Season/Summer_Season/Match_History")
+    # # Summer Playoff
+    # allData("https://lol.fandom.com/wiki/LCK/2023_Season/Summer_Season/Match_History")
+
+
+    urlList = ["https://lol.fandom.com/wiki/LCK/2023_Season/Spring_Season/Match_History", 
+               "https://lol.fandom.com/wiki/LCK/2023_Season/Spring_Playoffs/Match_History", 
+               "https://lol.fandom.com/wiki/LCK/2023_Season/Summer_Season/Match_History", 
+               "https://lol.fandom.com/wiki/LCK/2023_Season/Summer_Season/Match_History"]
+    
+    allTeams = []
+    allPlayers = []
+    allPicks = []
+    allWinSide = []
+    
+    for url in urlList:
+        info = allData(url)
+        allTeams.append(info[0])
+        allPlayers.append(info[1])
+        allPicks.append(info[2])
+        allWinSide.append(info[3])
+        print("ANDYYYYYY BIG")
+
+    allTeams = list(chain.from_iterable(allTeams))
+    allPlayers = list(chain.from_iterable(allPlayers))
+    allPicks = list(chain.from_iterable(allPicks))
+    allWinSide = list(chain.from_iterable(allWinSide))
+
+
+    blueTeam = []
+    redTeam = []
+    winnerTeam = []
+    
+    bluePlayerTop = []
+    bluePlayerJug = []
+    bluePlayerMid = []
+    bluePlayerBot = []
+    bluePlayerSup = []
+
+    redPlayerTop = []
+    redPlayerJug = []
+    redPlayerMid = []
+    redPlayerBot = []
+    redPlayerSup = []
+
+    blueChampTop= []
+    blueChampJug = []
+    blueChampMid = []
+    blueChampBot = []
+    blueChampSup = []
+
+    redChampTop = []
+    redChampJug = []
+    redChampMid = []   
+    redChampBot = []
+    redChampSup = []
+    
+    for i in range(len(allTeams)):
+        if(i%3 == 0): blueTeam.append(allTeams[i])
+        elif(i%3 == 1): redTeam.append(allTeams[i])
+        else: winnerTeam.append(allTeams[i])
+
+    for i in range(len(allPlayers)):
+        if(i%10 == 0): bluePlayerTop.append(allPlayers[i])
+        elif(i%10 == 1): bluePlayerJug.append(allPlayers[i])
+        elif(i%10 == 2): bluePlayerMid.append(allPlayers[i])
+        elif(i%10 == 3): bluePlayerBot.append(allPlayers[i])
+        elif(i%10 == 4): bluePlayerSup.append(allPlayers[i])
+        elif(i%10 == 5): redPlayerTop.append(allPlayers[i])
+        elif(i%10 == 6): redPlayerJug.append(allPlayers[i])
+        elif(i%10 == 7): redPlayerMid.append(allPlayers[i])
+        elif(i%10 == 8): redPlayerBot.append(allPlayers[i])
+        else: bluePlayerSup.append(allPlayers[i])
+
+    for i in range(len(allPicks)):
+        if(i%10 == 0): blueChampTop.append(allPicks[i])
+        elif(i%10 == 1): blueChampJug.append(allPicks[i])
+        elif(i%10 == 2): blueChampMid.append(allPicks[i])
+        elif(i%10 == 3): blueChampBot.append(allPicks[i])
+        elif(i%10 == 4): blueChampSup.append(allPicks[i])
+        elif(i%10 == 5): redChampTop.append(allPicks[i])
+        elif(i%10 == 6): redChampJug.append(allPicks[i])
+        elif(i%10 == 7): redChampMid.append(allPicks[i])
+        elif(i%10 == 8): redChampBot.append(allPicks[i])
+        else: bluePlayerSup.append(allPicks[i])
+
+    
+    df = pd.DataFrame(list(zip(blueTeam, redTeam, winnerTeam, allWinSide, bluePlayerTop, 
+    bluePlayerJug, bluePlayerMid, bluePlayerBot, bluePlayerSup, redPlayerTop, redPlayerJug, 
+    redPlayerMid, redPlayerBot, redPlayerSup, blueChampTop, blueChampJug, blueChampMid, 
+    blueChampBot, blueChampSup, redChampTop, redChampJug, redChampMid, redChampBot, redChampSup)), 
+    columns =['blueTeam', 'redTeam', 'winnerTeam', 'winnerSide', 'bluePlayerTop', 'bluePlayerJug', 
+    'bluePlayerMid', 'bluePlayerBot', 'bluePlayerSup', 'redPlayerTop',	'redPlayerJug', 'redPlayerMid',	
+    'redPlayerBot',	'redPlayerSup', 'blueChampTop', 'blueChampJug', 'blueChampMid', 'blueChampBot', 
+    'blueChampSup', 'redChampTop', 'redChampJug', 'redChampMid', 'redChampBot', 'redChampSup'])
+
+    df.to_csv('lol_data.csv', sep='\t', index = False)
 
 def allData(url):
     options = Options()
@@ -23,6 +125,7 @@ def allData(url):
     html = driver.page_source.encode('utf-8').strip()
     soup = BeautifulSoup(html, 'lxml')
 
+    info = []
     # Format: [Blue, Red, Winner, Blue, Red, Winner, Blue ...]
     teamsList = scrapeTeams(soup) 
 
@@ -34,7 +137,14 @@ def allData(url):
 
     # Blue or Red
     winSideList = winnerSide(teamsList)
-    print(teamsList)
+
+    info.append(teamsList)
+    info.append(playerList)
+    info.append(picksList)
+    info.append(winSideList)
+
+    return info
+
 
 def scrapeTeams(soup):
     teamNameData = soup.find(class_ = "table-wide").find_all('img', alt = True)
